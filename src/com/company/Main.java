c4a6dc3f503c63cf15406930a824cfa7900b65fb
jim88465 <jim88465@gmail.com>
2017-09-25 14:44:46 +0800

    aaa

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("103024095黃瑞裕");
+        System.out.println("資訊傳播學系");
+    }
+}
